dce5bee1f61f669e14a5e28e95cdb75d3ab219fc
Aryan Kamboj <99310915+Nemmen@users.noreply.github.com>
2024-09-22 12:04:06 +0000

    new

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Aryan Kamboj",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
   gitHub: "microsoft",
@@ -32,7 +32,7 @@ const siteProps = {
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "microsoft",
 };
 
 const primaryColor = "#4E567E";
